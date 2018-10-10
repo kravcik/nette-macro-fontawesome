3a0551cf02fd1789e2ba94cbf751d7290d5c518d
Pavel Kravčík <kravcik@modulservis.cz>
2018-10-10 13:58:59 +0200

    Add secondary color BS4 + default FA5 'fal' style

--- src/FontAwesomeMacro.php
+++ src/FontAwesomeMacro.php
@@ -13,12 +13,13 @@ class FontAwesomeMacro extends \Latte\Macros\MacroSet
         'danger',
         'warning',
         'default',
-        'primary'
+        'primary',
+        'secondary'
     ];
-    
+
     /**
      * Install macro
-     * 
+     *
      */
     public static function install(\Latte\Compiler $compiler)
     {
@@ -31,10 +32,10 @@ class FontAwesomeMacro extends \Latte\Macros\MacroSet
 
     /**
      * Custom icon macro
-     * 
+     *
      * @param \Latte\MacroNode $node
      * @param \Latte\PhpWriter $writer
-     * 
+     *
      * @return string
      */
     public function icon(\Latte\MacroNode $node, \Latte\PhpWriter $writer)
@@ -47,34 +48,34 @@ class FontAwesomeMacro extends \Latte\Macros\MacroSet
     /**
      * Render font-awesome icon
      * @note Options can be set by like associate array or like simple array in right order
-     * 
+     *
      * @param string $icon
      * @param array $arguments (see options)
-     * 
+     *
      * @option string color (set color - e.g. red) - generate color-red || ignore (bootstrap class like success
      * @option int size (set size - e.g. 2) - generate 2x
      * @option bool fixed-width (default TRUE)
-     * 
+     *
      * @return \Nette\Utils\Html
      */
     public static function renderIcon($icon, array $arguments)
     {
         $isIndex = array_values($arguments) === $arguments;
-        
+
         $el = \Nette\Utils\Html::el(isset($arguments['el']) ? $arguments['el'] : 'span');
         $class = [];
-        $class[] = 'fa fa-' . $icon;
-        
+        $class[] = 'fal fa-' . $icon;
+
         /**
          * Color argument
          */
         if(isset($arguments['color']) || ($isIndex && isset($arguments[0])))
         {
             $color = isset($arguments['color']) ? $arguments['color'] : $arguments[0];
-                        
+
             $class[] = in_array($color, self::$colorClass) ? 'text-' . $color : 'color-' . $color;
         }
-        
+
         /**
          * Size argument
          */
@@ -83,7 +84,7 @@ class FontAwesomeMacro extends \Latte\Macros\MacroSet
             $size = isset($arguments['size']) ? $arguments['size'] : $arguments[1];
             $class[] =  'fa-' . $size . 'x';
         }
-        
+
         /**
          * Fixed width argument
          */
@@ -91,9 +92,9 @@ class FontAwesomeMacro extends \Latte\Macros\MacroSet
         {
             $class[] = 'fa-fw';
         }
-        
+
         $el->addAttributes(['class' => $class]);
-        
+
         return $el;
     }
 }
