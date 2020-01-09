faad618a8225addf7ca8a17fe27f9612dcad5f8c
Pavel Kravčík <kravcik@modulservis.cz>
2020-01-09 13:06:53 +0100

    CS

--- src/FontAwesomeMacro.php
+++ src/FontAwesomeMacro.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 /**
  * @link https://github.com/kravcik/nette-macro-fontawesome
@@ -8,112 +9,114 @@ namespace Kravcik\Macros;
 
 class FontAwesomeMacro extends \Latte\Macros\MacroSet
 {
-    /**
-     * BS4 colors
-     */
-    private static $colorClass = [
-        'primary',
-        'secondary',
-        'success',
-        'danger',
-        'warning',
-        'info',
-        'light',
-        'dark',
-        'body',
-        'muted',
-        'white',
-        'black-50',
-        'white-50'
-    ];
+	/**
+	 * BS4 colors
+	 */
+	private static $colorClass = [
+		'primary',
+		'secondary',
+		'success',
+		'danger',
+		'warning',
+		'info',
+		'light',
+		'dark',
+		'body',
+		'muted',
+		'white',
+		'black-50',
+		'white-50'
+	];
 
-    
-    /**
-     * Install macro
-     */
-    public static function install(\Latte\Compiler $compiler)
-    {
-        $set = new static($compiler);
 
-        $set->addMacro('icon', [$set, 'icon']);
+	/**
+	 * Install macro
+	 */
+	public static function install(\Latte\Compiler $compiler)
+	{
+		$set = new static($compiler);
 
-        return $set;
-    }
+		$set->addMacro('icon', [$set, 'icon']);
 
-    /**
-     * Custom icon macro          
-     */
-    public function icon(\Latte\MacroNode $node, \Latte\PhpWriter $writer): string
-    {
-        return $writer->write(
-            'echo \Kravcik\Macros\FontAwesomeMacro::renderIcon(%node.word, %node.array)'
-        );
-    }
+		return $set;
+	}
 
-    /**
-     * Render font-awesome icon
-     * @note Options can be set by like associate array or like simple array in right order
-     *     
-     *
-     * @option string color (set color - e.g. red) - generate color-red || ignore (bootstrap class like success
-     * @option int size (set size - e.g. 2) - generate 2x
-     * @option bool fixed-width (default true)
-     */
-    public static function renderIcon(string $icon, array $arguments): \Nette\Utils\Html
-    {
-        $isIndex = array_values($arguments) === $arguments;
 
-        $el = \Nette\Utils\Html::el($arguments['el'] ?? 'span');
-        $class = [];
+	/**
+	 * Custom icon macro          
+	 */
+	public function icon(\Latte\MacroNode $node, \Latte\PhpWriter $writer): string
+	{
+		return $writer->write(
+			'echo \Kravcik\Macros\FontAwesomeMacro::renderIcon(%node.word, %node.array)'
+		);
+	}
 
-        /**
-         * Color argument
-         */
-        if(isset($arguments['color']) || ($isIndex && isset($arguments[0])))
-        {
-            $color = $arguments['color'] ?? $arguments[0];
 
-            $class[] = in_array($color, self::$colorClass) ? 'text-' . $color : 'color-' . $color;
-        }
+	/**
+	 * Render font-awesome icon
+	 * @note Options can be set by like associate array or like simple array in right order
+	 *     
+	 *
+	 * @option string color (set color - e.g. red) - generate color-red || ignore (bootstrap class like success
+	 * @option int size (set size - e.g. 2) - generate 2x
+	 * @option bool fixed-width (default true)
+	 */
+	public static function renderIcon(string $icon, array $arguments): \Nette\Utils\Html
+	{
+		$isIndex = array_values($arguments) === $arguments;
 
-        /**
-         * Size argument
-         */
-        if(isset($arguments['size']) || ($isIndex && isset($arguments[1])))
-        {
-            $size = $arguments['size'] ?? $arguments[1];
-            
-            if(is_numeric($size))
-            {
-                $class[] =  'fa-' . $size . 'x';
-            }
-            else
-            {
-                $class[] =  'fa-' . $size;
-            }
-        }
+		$el = \Nette\Utils\Html::el($arguments['el'] ?? 'span');
+		$class = [];
 
-        /**
-         * Fixed width argument
-         */
-        if((!isset($arguments['fw']) && !$isIndex ) || ($isIndex && !isset($arguments[2])))
-        {
-            $class[] = 'fa-fw';
-        }
+		/**
+		 * Color argument
+		 */
+		if(isset($arguments['color']) || ($isIndex && isset($arguments[0])))
+		{
+			$color = $arguments['color'] ?? $arguments[0];
 
-        if(isset($arguments['style']) || ($isIndex && isset($arguments[3])))
-        {
-            $style = $arguments['style'] ?? $arguments[3];
-        }
-        else
-        {
-            $style = 'fal';
-        }
+			$class[] = in_array($color, self::$colorClass, true) ? 'text-' . $color : 'color-' . $color;
+		}
 
-        array_unshift($class, $style . ' fa-' . $icon);
+		/**
+		 * Size argument
+		 */
+		if(isset($arguments['size']) || ($isIndex && isset($arguments[1])))
+		{
+			$size = $arguments['size'] ?? $arguments[1];
 
-        $el->addAttributes(['class' => $class]);
+			if(is_numeric($size))
+			{
+				$class[] = 'fa-' . $size . 'x';
+			}
+			else
+			{
+				$class[] = 'fa-' . $size;
+			}
+		}
 
-        return $el;
-    }
+		/**
+		 * Fixed width argument
+		 */
+		if((!isset($arguments['fw']) && !$isIndex) || ($isIndex && !isset($arguments[2])))
+		{
+			$class[] = 'fa-fw';
+		}
+
+		if(isset($arguments['style']) || ($isIndex && isset($arguments[3])))
+		{
+			$style = $arguments['style'] ?? $arguments[3];
+		}
+		else
+		{
+			$style = 'fal';
+		}
+
+		array_unshift($class, $style . ' fa-' . $icon);
+
+		$el->addAttributes(['class' => $class]);
+
+		return $el;
+	}
 }
