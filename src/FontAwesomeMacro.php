93b3f8b90b3fe972fb3c2a0ab6252ccde5878515
Pavel Kravčík <kravcik@modulservis.cz>
2020-01-09 13:04:12 +0100

    Update to nette3

--- src/FontAwesomeMacro.php
+++ src/FontAwesomeMacro.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @link https://github.com/kravcik/nette-macro-fontawesome
  */
@@ -7,19 +8,28 @@ namespace Kravcik\Macros;
 
 class FontAwesomeMacro extends \Latte\Macros\MacroSet
 {
+    /**
+     * BS4 colors
+     */
     private static $colorClass = [
+        'primary',
+        'secondary',
         'success',
-        'info',
         'danger',
         'warning',
-        'default',
-        'primary',
-        'secondary'
+        'info',
+        'light',
+        'dark',
+        'body',
+        'muted',
+        'white',
+        'black-50',
+        'white-50'
     ];
 
+    
     /**
      * Install macro
-     *
      */
     public static function install(\Latte\Compiler $compiler)
     {
@@ -31,14 +41,9 @@ class FontAwesomeMacro extends \Latte\Macros\MacroSet
     }
 
     /**
-     * Custom icon macro
-     *
-     * @param \Latte\MacroNode $node
-     * @param \Latte\PhpWriter $writer
-     *
-     * @return string
+     * Custom icon macro          
      */
-    public function icon(\Latte\MacroNode $node, \Latte\PhpWriter $writer)
+    public function icon(\Latte\MacroNode $node, \Latte\PhpWriter $writer): string
     {
         return $writer->write(
             'echo \Kravcik\Macros\FontAwesomeMacro::renderIcon(%node.word, %node.array)'
@@ -48,21 +53,17 @@ class FontAwesomeMacro extends \Latte\Macros\MacroSet
     /**
      * Render font-awesome icon
      * @note Options can be set by like associate array or like simple array in right order
-     *
-     * @param string $icon
-     * @param array $arguments (see options)
+     *     
      *
      * @option string color (set color - e.g. red) - generate color-red || ignore (bootstrap class like success
      * @option int size (set size - e.g. 2) - generate 2x
-     * @option bool fixed-width (default TRUE)
-     *
-     * @return \Nette\Utils\Html
+     * @option bool fixed-width (default true)
      */
-    public static function renderIcon($icon, array $arguments)
+    public static function renderIcon(string $icon, array $arguments): \Nette\Utils\Html
     {
         $isIndex = array_values($arguments) === $arguments;
 
-        $el = \Nette\Utils\Html::el(isset($arguments['el']) ? $arguments['el'] : 'span');
+        $el = \Nette\Utils\Html::el($arguments['el'] ?? 'span');
         $class = [];
 
         /**
@@ -70,7 +71,7 @@ class FontAwesomeMacro extends \Latte\Macros\MacroSet
          */
         if(isset($arguments['color']) || ($isIndex && isset($arguments[0])))
         {
-            $color = isset($arguments['color']) ? $arguments['color'] : $arguments[0];
+            $color = $arguments['color'] ?? $arguments[0];
 
             $class[] = in_array($color, self::$colorClass) ? 'text-' . $color : 'color-' . $color;
         }
@@ -80,7 +81,8 @@ class FontAwesomeMacro extends \Latte\Macros\MacroSet
          */
         if(isset($arguments['size']) || ($isIndex && isset($arguments[1])))
         {
-            $size = isset($arguments['size']) ? $arguments['size'] : $arguments[1];
+            $size = $arguments['size'] ?? $arguments[1];
+            
             if(is_numeric($size))
             {
                 $class[] =  'fa-' . $size . 'x';
@@ -101,7 +103,7 @@ class FontAwesomeMacro extends \Latte\Macros\MacroSet
 
         if(isset($arguments['style']) || ($isIndex && isset($arguments[3])))
         {
-            $style = isset($arguments['style']) ? $arguments['style'] : $arguments[3];
+            $style = $arguments['style'] ?? $arguments[3];
         }
         else
         {
